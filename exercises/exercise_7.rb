e569cc8918717465f0dc1551233e4cdd12508733
siopao2001 <sio_pao2001@yahoo.ca>
2022-08-27 17:48:24 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+print "Type in the store name >"
+@userchoice = $stdin.gets.chomp
+store_7 = Store.new
+store_7.name ="#{@userchoice}"
+store_7.save
+puts store_7.valid?
+puts store_7.errors.objects.first.full_message
+
+# create_record
+
+#store_7 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+# store_8 = Store.create(name: "Burnaby")
+# puts store_8.valid?
+# puts store_8.errors.objects.first.full_message
