be3c8a3d792fa5b08fe572340131b6816edffb8b
siopao2001 <sio_pao2001@yahoo.ca>
2022-08-27 19:47:39 +0000

    Modify validation for stores

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
-  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validates :annual_revenue, presence: true
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}, presence: true
+  
 end
